ee5594f6265577d0088a7b0390c3828aa67cf65a
manjunath <manjunatha.m@techjini.com>
2017-12-12 11:21:30 +0530

    GET POST GETbyid

--- server/server2.js
+++ server/server2.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID}  =require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -19,6 +20,34 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos',(req, res)=>{
+  Todo.find().then((todos)=>{
+  res.send({todos})
+  },(err)=>{
+    res.status(400).send(err);
+  })
+});
+
+app.get('/todos/:id',(req, res)=>{
+  var id = req.params.id;
+  //res.send(id);
+  if(!ObjectID.isValid(id))
+     res.status(404).send('Invalid id');
+  
+  Todo.findById(id).then((todo)=>{
+    if(!todo)
+      return res.status(404).send('No data found for this id');
+    res.send(JSON.stringify(todo, undefined, 2));
+  },(err)=>{
+     return res.status(404);
+  }).catch((e)=>{return res.status(404)});
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
 });
 
 app.listen(3000, () => {
--- server/tests/server2.test.js
+++ server/tests/server2.test.js
@@ -1,11 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} =require('mongodb');
 const {app} = require('./../server2');
 const {Todo} = require('./../models/todo');
 
+var todos = [{_id:new ObjectID(),text:'todo1'},{_id:new ObjectID(),text:'todo2'}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+      return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +28,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +47,49 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
   });
+});
+
+describe('GET /todos',()=>{
+    it('Should return all the  todos',(done)=>{
+        request(app)
+        .get('/todos')
+       .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(JSON.parse(res.text).text).toBe(todos[0].text);
+      },(err)=>{
+        done();
+      })
+      .end(done);
+  });
+
+    it('should return 404 for id not found',(done)=>{
+        request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for invalid id',(done)=>{
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
+    });
 });
